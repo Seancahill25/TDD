664323730f78d223bd38ce1d93e37e8af9afb265
Sean Cahill <seancahill25@gmail.com>
2018-10-30 12:06:32 -0400

    started TDD work

---
+++ TDDpractice.UnitTests/StringUtilsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TDDpractice.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTests
+    {
+        [Test]
+        public void CountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome.";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+            var stringUtils = new StringUtils();
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CountNumberOfLettersInComplexSentence()
+         {
+            var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+            var characterToScanFor = "n";
+            var expectedResult = 5;
+            var stringUtils = new StringUtils();
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+ 
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
