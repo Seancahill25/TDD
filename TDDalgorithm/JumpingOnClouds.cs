a25c51fc114a15f95e5a77da401434e1550cf9e1
Sean Cahill <seancahill25@gmail.com>
2018-11-03 23:08:00 -0400

    TDD algorithm

---
+++ TDDalgorithm/JumpingOnClouds.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDalgorithm
+{
+    public class JumpingOnClouds
+    {
+        public static int Calculate(int [] cloudsArray)
+        {   
+            int jumps = 0;
+            
+                //for (var i = 0; i < cloudsArray.Length - 1; i++)
+                //{
+                //    jumps++;
+                //    if(cloudsArray[i] == 1 || cloudsArray[i + 2])
+                //    {
+                //        jumps--;
+                //    }
+                //}
+            
+            for (var i = 0; i < cloudsArray.Length - 1; i++)
+            {
+                if (i < cloudsArray.Length - 2 && cloudsArray[i+2] == 0)
+                {
+                    i++;
+                    jumps++;
+                }
+                else
+                {
+                    jumps++;
+                }
+            }
+
+           
+
+            return jumps;
+        }
+    }
+}
---
+++ TDDalgorithm/JumpingOnCloudsTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDalgorithm
+{
+    [TestFixture]
+    class JumpingOnCloudsTest
+    {
+        [Test]
+        public void InputLengthOneReturnsZero()
+        {
+            int[] cloudsArray = { 0 };
+            int expected = 0;
+            Assert.AreEqual(JumpingOnClouds.Calculate(cloudsArray), expected);
+            
+        }
+
+        [Test]
+        public void InputLengthTwoReturnsOne()
+        {
+            int[] cloudsArray = { 0, 0 };
+            int expected = 1;
+            Assert.AreEqual(JumpingOnClouds.Calculate(cloudsArray), expected);
+        }
+
+        [Test]
+        public void JumpOverOneCloud()
+        {
+            int[] cloudsArray = { 0, 1, 0 };
+            int expected = 1;
+            
+        }
+
+        [Test]  
+        public void JumpOverMultipleClouds()
+        {
+            int[] cloudsArray = { 0, 0, 1, 0, 0, 1, 0 };
+            int expected = 4;
+            Assert.AreEqual(JumpingOnClouds.Calculate(cloudsArray), expected);
+        }
+
+        [Test]
+        public void JumpOverMultipleZeros()
+        {
+            int[] cloudsArray = { 0, 0, 0, 1, 0, 0 };
+            int expected = 3;
+            Assert.AreEqual(JumpingOnClouds.Calculate(cloudsArray), expected);
+        }
+
+    }
+}
