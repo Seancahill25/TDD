280da8e71698ea60bbae77efe22d0c6a59509fca
Sean Cahill <seancahill25@gmail.com>
2018-11-10 16:08:49 -0500

    Did Roman Numeral Coverter using TDD

---
+++ RomanNumeralConverter/Converter.Test.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralConverter
+{
+    [TestFixture]
+    public class ConverterTest
+    {
+        Converter converter = new Converter();
+       [Test]
+       public void EmptyStringReturns0()
+        {
+            string input = "";
+            int expectedOutput = 0;
+
+            Assert.AreEqual(converter.ConvertNumerals(input), expectedOutput);
+        }
+
+        [Test]
+        public void NumeralIReturnsOne()
+        {
+            string input = "I";
+            int expectedOutput = 1;
+
+            Assert.AreEqual(expectedOutput, converter.ConvertNumerals(input));
+        }
+
+        [Test]
+        public void NumeralVReturnsFive()
+        {
+            string input = "V";
+            int expectedOutput = 5;
+
+            Assert.AreEqual(converter.ConvertNumerals(input), expectedOutput);
+        }
+
+        [Test]
+        public void AddMultipleNumeralsTogether()
+        {
+            string input = "VI";
+            int expectedOutput = 6;
+
+            Assert.AreEqual(converter.ConvertNumerals(input), expectedOutput);
+        }
+
+        [Test]
+        public void IVshouldSubtractAndReturnFour()
+        {
+            string input = "IV";
+            int expectedOutput = 4;
+
+            Assert.AreEqual(converter.ConvertNumerals(input), expectedOutput);
+        }
+
+        [Test]
+        public void LargeRomanNumeralShouldReturnProperNumber()
+        {
+            string input = "MCMXLIV";
+            int expectedOutput = 1944;
+
+            Assert.AreEqual(converter.ConvertNumerals(input), expectedOutput);
+        }
+    }
+}
---
+++ RomanNumeralConverter/Converter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralConverter
+{
+    class Converter
+    {
+       public static Dictionary<char, int> romanValues = new Dictionary<char, int>()
+       {
+           {'I', 1 },
+           {'V', 5 },
+           {'X', 10},
+           {'L', 50},
+           {'C', 100},
+           {'D', 500},
+           {'M', 1000}
+       };
+        public int ConvertNumerals(string romanNumerals)
+        {   
+            int Converted = 0;  
+            for(var i = 0; i < romanNumerals.Length; i++)
+            {
+                if(i + 1 < romanNumerals.Length  && romanValues[romanNumerals[i]] < romanValues[romanNumerals[i + 1]])
+                {
+                    Converted -= romanValues[romanNumerals[i]];
+                } else
+                {
+                    Converted += romanValues[romanNumerals[i]];
+                }
+            }
+            return Converted;
+        }
+    }
+}
