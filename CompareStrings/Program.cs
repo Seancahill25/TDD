becc92e497937f2ee181ea366ffbf9bbfad8de35
Sean Cahill <seancahill25@gmail.com>
2019-01-07 10:25:08 -0500

    added function that calculates differences in two strings

---
+++ CompareStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = "this";
+            var string2 = "that";
+            int[] total = new int[string1.Length];
+
+            string1.ToCharArray();
+            string2.ToCharArray();
+
+            for(var i = 0; i < string1.Length; i++)
+            {
+                total[i] = string1[i] - string2[i];
+            }
+            Console.WriteLine(total);
+            foreach(var i in total)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
