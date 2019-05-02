c8ad6e5588612025179ea570243524ceaf914f5b
Sean Cahill <seancahill25@gmail.com>
2019-05-02 12:36:03 -0400

    checks if Date is a holiday and returns next business day.

---
+++ DueDate/DueDate/Bill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PaymentDateCheck
+{
+    public class Bill
+    {
+        private IHolidayService _holidayService = new HolidayService();
+        
+        public DateTime CheckDate (DateTime dueDate)
+        {
+            if (dueDate.DayOfWeek == DayOfWeek.Saturday)
+            {
+                return dueDate.AddDays(2);
+            }
+
+            if (dueDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return dueDate.AddDays(1);
+            }
+
+            if (_holidayService.isHoliday(dueDate) && dueDate.DayOfWeek == DayOfWeek.Friday)
+            {
+                return dueDate.AddDays(3);
+            }
+            else if (_holidayService.isHoliday(dueDate))
+            {
+                return dueDate.AddDays(1);
+            }
+            return dueDate;
+        }
+    }
+
+}
+
