1b28e7428d271078d9c9f23e4cb3d771a63154fd
Sean Cahill <seancahill25@gmail.com>
2019-06-20 13:41:29 -0400

    algorithm that delets all what space characters that do not start a string.

---
+++ WhiteSpaceTrimmer/WhiteSpaceTrimmer/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TrimSpaces
+    {
+        public static string CheckForWhiteSpace(string input)
+        {
+            StringBuilder newstring = new StringBuilder(input);
+            bool firstLetterFound = false;
+            if (input == null)
+                throw new NullReferenceException();
+
+            if (input == string.Empty)
+                throw new ArgumentException();
+
+            for(var i = 0; i < newstring.Length; i++)
+            {
+                if (firstLetterFound == false && !Char.IsWhiteSpace(newstring[i]))
+                    firstLetterFound = true;
+                
+                if (firstLetterFound == true && newstring[i] == ' ' || newstring[i] == '\t')
+                    newstring[i] = 'x';
+            }
+            return newstring.Replace("x","").ToString();
+        }
+    }
+}
